92f289f81dc4f45741b856161cd62a8d208cbe1b
macmacal <mac.aleksandrowicz@gmail.com>
2026-07-14 12:21:09 +0200

    Added licence headers

---
+++ wled_ros_driver/wled_ros_driver/__init__.py
@@ -0,0 +1,4 @@
+# Copyright (c) 2025-2026, AGH Center of Excellence in Artificial Intelligence
+# All rights reserved.
+#
+# SPDX-License-Identifier: Apache-2.0
--- wled_ros_driver/wled_ros_driver/start_wled_service.py
+++ wled_ros_driver/wled_ros_driver/start_wled_service.py
@@ -1,3 +1,8 @@
+# Copyright (c) 2025-2026, AGH Center of Excellence in Artificial Intelligence
+# All rights reserved.
+#
+# SPDX-License-Identifier: Apache-2.0
+
 import asyncio
 from wled import WLED
 
