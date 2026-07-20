84964309af8483c0fcccc7e8e8346d907faea245
Norbi <norbercik.boron@gmail.com>
2026-07-20 11:39:01 +0200

    added Color classmethod for list initialization

--- wled_ros_driver/wled_ros_driver/types/color_data.py
+++ wled_ros_driver/wled_ros_driver/types/color_data.py
@@ -2,14 +2,19 @@
 # All rights reserved.
 #
 # SPDX-License-Identifier: Apache-2.0
-from typing import NamedTuple
-
+from typing import NamedTuple,Sequence
 
 # 1. Define the named tuple structure
 class Color(NamedTuple):
     R: int
     G: int
     B: int
+
+    @classmethod
+    def from_list(cls, values: Sequence[int]) -> "Color":
+        if len(values) != 3:
+            raise ValueError("Color must have exactly 3 components: R, G, B")
+        return cls(*values)
 
     def __str__(self) -> str:
         return f"R:{self.R} G:{self.G} B:{self.B}"
